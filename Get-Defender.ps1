7b29bca38039d4782176a0edb7fdfc5f7e8458a9
Salman Ahmed <86254894+ahmedsalmantec@users.noreply.github.com>
2024-03-21 20:02:02 +0400

    Update Get-Defender.ps1

--- Get-Defender.ps1
+++ Get-Defender.ps1
@@ -1,3 +1,9 @@
+# PS-Get-Defender
+# This powershell function is intended to detect the status of Microsoft Defender on end-point devices.
+# Last Updated: 2024-03-21
+
+
+# To use this run the command "Get-MSD"
 
 
 function Get-MSD {
